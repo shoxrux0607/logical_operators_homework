771c23303a64a76332d125bb9bbea310fb90f1c9
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 18:07:37 +0500

    07

--- logic07.py
+++ logic07.py
@@ -1,10 +1,13 @@
 def main(a,b):
     """
-    Given two integers a, b,  check the following statement "At least one  of the numbers 'a' and 'b' is negative".
+    Given two integers a, b, 
+     check the following statement 
+     "At least one  of the numbers 'a' and 'b' is negative".
     Args:
         a(int): parameter a
         b(int): parameter b
     Returns:
         bool: answer
     """
-    return
+    return a<0 or b<0
+print(main(3,1))
