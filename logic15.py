1a9a75a2f7dc5765174425165d85525ed444c438
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 19:50:26 +0500

    15

--- logic15.py
+++ logic15.py
@@ -1,9 +1,19 @@
 def main(a):
     """
-    Given a three-digit integer a,  check the following statement "All digits sum is odd".
+    Given a three-digit integer a, 
+     check the following statement "All digits sum is odd".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    
+    x = a % 10
+    y = a // 10 % 10
+    z = a // 100
+    c = x + y + z
+
+    return c % 2 != 0
+
+
+print(main(322))
