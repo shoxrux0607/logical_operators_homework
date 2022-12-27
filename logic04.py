29bcef675666e62a43cf0c572a820af7d7961624
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 17:59:39 +0500

    04

--- logic04.py
+++ logic04.py
@@ -1,10 +1,14 @@
-def main(a,b):
+def main(a, b):
     """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is even".
+    Given two integers a, b, 
+     check the following statement "Each of the numbers 'a' and 'b' is even".
     Args:
         a(int): parameter a
         b(int): parameter b
     Returns:
         bool: answer
     """
-    return
+    return a % 2 == 0 and b % 2 == 0
+
+
+print(main(3, 6))
