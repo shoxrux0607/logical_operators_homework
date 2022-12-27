5cc64a976013a3264511ae97dd47ae6501cff971
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 17:54:53 +0500

    02

--- logic02.py
+++ logic02.py
@@ -1,10 +1,14 @@
-def main(a,b):
+def main(a, b):
     """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is positive".
+    Given two integers a, b,  check the following statement 
+    "Each of the numbers 'a' and 'b' is positive".
     Args:
         a(int): parameter a
         b(int): parameter b
     Returns:
         bool: answer
     """
-    return
+
+    return a > 0 and b > 0
+
+print(main(4,5))
